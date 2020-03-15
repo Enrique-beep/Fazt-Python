fe476c43cd76e38931d8a2580d2d474256705703
Henry <contrerasenrique973@gmail.com>
2020-03-15 13:49:03 -0700

    Metodos de listas

---
+++ lists.py
@@ -0,0 +1,9 @@
+test_list = list(range(0,10))
+
+# print(dir(numbers_list))
+
+## Métodos de una lista ##
+
+print(f"Número de elementos de la lista: {len(test_list)}")				# Cuenta el numero de elementos de una lista
+print(f"Existe el numero 5 en la lista: test_list {5 in test_list}")	# Devuelve true o false dependiendo de la sentencia <in> 
+test_list.append('francois')											# Inserta al final uno o varios elementos de cualquier tipo a la lista
