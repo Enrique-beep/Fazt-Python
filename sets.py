152ef961c251a8faca1d7116af777298338c4397
Henry <contrerasenrique973@gmail.com>
2020-03-15 14:39:47 -0700

    Inicio con diccionarios

--- sets.py
+++ sets.py
@@ -1,1 +1,13 @@
-## sets --> Conjuntos
+## sets --> Conjuntos desordenados
+
+#conjuto = set()
+conjuto = {'Red', 'Blue', 'Green'}
+
+## Metódos de los conjuntos ##
+
+conjuto.add('Violet')				## Inserta un elemento a un conjunto
+conjuto.remove('Violet')			## Remueve un elemento de un conjunto
+conjuto.clear()						## Remueve todos los elementos del conjunto
+#del conjunto 						## Elimina el conjunto <<Sirve para cualquier dato>>
+
+#print(f"conjuto = {conjuto}")
